9cdb1c09b86449dbc71fbdfc190f719cf98e5cd9
jagritisurabhi <jagritisurabhi17@gmail.com>
2024-01-28 19:12:18 +0530

    Added test cases

--- shoppingcart.py
+++ shoppingcart.py
@@ -18,4 +18,9 @@ class ShoppingCart:
         return self.items
 
     def get_total_price(self, price_map):
-        pass
+        total_price = 0
+        for item in self.items:
+            total_price += price_map.get(
+                item
+            )  # Using dictionary get method here which returns the value of the key
+        return total_price
--- test_shoppingcart.py
+++ test_shoppingcart.py
@@ -2,23 +2,35 @@ import pytest
 from shoppingcart import ShoppingCart
 
 
-# Scenario 1: Possible to add items to a shopping cart
-def test_can_add_items_to_cart():
-    cart = ShoppingCart(5)
+@pytest.fixture
+def cart():
+    """All setup code here, needed to run unit tests"""
+    return ShoppingCart(5)
+
+
+"""Scenario 1: Possible to add items to a shopping cart"""
+
+
+def test_can_add_items_to_cart(cart):
+    # cart = ShoppingCart(5) - REPLACED BY FIXTURE
     cart.add("apple")
     assert cart.size() == 1
 
 
-# Scenario 2: Added item is in the cart, we use method get_items() to check
-def test_when_item_added_then_cart_contains_item():
-    cart = ShoppingCart(5)
+"""Scenario 2: Added item is in the cart, we use method get_items() to check"""
+
+
+def test_when_item_added_then_cart_contains_item(cart):
+    # cart = ShoppingCart(5) - REPLACED BY FIXTURE
     cart.add("apple")
     assert "apple" in cart.get_items()
 
 
-# Scenario 3: Adding test case to test an Exception by checking around max size
-def test_when_add_more_than_max_items_should_fail():
-    cart = ShoppingCart(5)
+"""Scenario 3: Adding test case to test an Exception by checking around max size"""
+
+
+def test_when_add_more_than_max_items_should_fail(cart):
+    # cart = ShoppingCart(5) - REPLACED BY FIXTURE
     for _ in range(5):
         cart.add("apple")
         # Happy flow, saying it is possible to add 5 items since max_size is 5
@@ -28,4 +40,14 @@ def test_when_add_more_than_max_items_should_fail():
         # Failure flow, caught error adding above threshold(max_size)
 
 
-# Scenario 4: Getting the price of items in the cart
+"""Scenario 4: Getting the price of items in the cart"""
+
+
+def test_can_get_total_price(cart):
+    # cart = ShoppingCart(5) - REPLACED BY FIXTURE
+    cart.add("pineapple")
+    cart.add("watermelon")
+
+    # price_map: Mentioned in shopping cart, we define it here as a Dictionary object
+    price_map = {"apple": 1.00, "orange": 1.00, "pineapple": 2.00, "watermelon": 1.95}
+    assert cart.get_total_price(price_map) == 3.95
